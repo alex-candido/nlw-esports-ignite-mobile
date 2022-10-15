483e51760aee6387a4313095404bd860c237a61f
Alex Cândido <alex.candido.tec@gmail.com>
2022-10-14 21:44:51 -0300

    estrutura Heading

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Image, View } from 'react-native';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
+import { Heading } from '../../components/Heading';
 
 import { styles } from './styles';
 
@@ -11,6 +12,11 @@ export function Home() {
       <Image 
         source={logoImg}
         style={styles.logo}
+      />
+
+      <Heading 
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
       />
     </View>
   );
